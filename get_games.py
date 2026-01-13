b54bc468ad88b5f1e929ac85aeb84c982ba00843
Alan Silva <alanmotagomes@gmail.com>
2026-01-12 22:31:32 -0300

    statr of the project with the methods request_page and get_last_page

---
+++ get_games.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+
+jogos = []
+
+def request_page(year):
+    link = f'https://www.metacritic.com/browse/game/all/all/{year}/metascore/'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'}
+    params = {'page':1}
+    request = requests.get(link,headers=headers,params=params)
+    page = BeautifulSoup(request.text, 'html.parser')
+    return page
+
+
+def get_last_page():
+    page = request_page(2025)
+    last_pages = page.find_all('span', class_='c-navigationPagination_itemButtonContent')
+    last_page_number = last_pages[-2].text.strip()
+    return int(last_page_number)
+
+
