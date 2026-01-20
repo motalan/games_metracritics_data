a875b2fa35dc706ef600506117e74b0cf637dfbf
Alan Silva <alanmotagomes@gmail.com>
2026-01-20 19:55:21 -0300

    finished all the information needed

--- get_game_info.py
+++ get_game_info.py
@@ -8,6 +8,7 @@ params = {'page':1}
 
 page = requests.get(link,params=params,headers=headers)
 page = BeautifulSoup(page.text,'html.parser')
+released_date = page.find_all('div', class_='g-text-xsmall')[1].find_all('span')[-1].text
 metascore_review = page.find('div', class_='c-siteReviewScore u-flexbox-column u-flexbox-alignCenter u-flexbox-justifyCenter g-text-bold c-siteReviewScore_green g-color-gray90 c-siteReviewScore_medium').text
 user_review = page.find('div', class_='c-siteReviewScore u-flexbox-column u-flexbox-alignCenter u-flexbox-justifyCenter g-text-bold c-siteReviewScore_green c-siteReviewScore_user g-color-gray90 c-siteReviewScore_medium').text
 platforms = page.find_all('li', class_='c-gameDetails_listItem g-color-gray70 u-inline-block')
@@ -15,4 +16,10 @@ platforms = [games.text.strip() for games in platforms]
 #developer = page.find('li', class_='c-gameDetails_listItem u-inline-block g-color-gray70').text
 #publisher = page.find('span', class_='g-outer-spacing-left-medium-fluid u-block g-color-gray70').text
 genres = page.find_all('span', class_='c-globalButton_label')[-2].text.strip().split(" ")
-print(genres)
+postive_critic = page.find('div', class_='c-reviewsStats_positiveStats').find_all('span')[-1].text.split(" ")[0]
+mixed_critic = page.find('div', class_='c-reviewsStats_neutralStats').find_all('span')[-1].text.split(" ")[0]
+negative_critic = page.find('div', class_='c-reviewsStats_negativeStats').find_all('span')[-1].text.split(" ")[0]
+positive_user = page.find_all('div', class_='c-reviewsStats_positiveStats')[2].find_all('span')[-1].text.split(" ")[0]
+mixed_user = page.find_all('div', class_='c-reviewsStats_neutralStats')[2].find_all('span')[-1].text.split(" ")[0]
+negative_user = page.find_all('div', class_='c-reviewsStats_negativeStats')[2].find_all('span')[-1].text.split(" ")[0]
+print(released_date)
