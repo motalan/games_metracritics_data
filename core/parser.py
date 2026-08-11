0cd299facab192d184c55ede98a7b1d3d72c8856
Alan Mota <alanmotagomes@gmail.com>
2026-08-11 17:54:51 -0300

    Last_page function removed and added get_game_info

--- core/parser.py
+++ core/parser.py
@@ -1,24 +1,12 @@
 from bs4 import BeautifulSoup
 
-
+# Souping function to parse HTML content
 def souping (text_page: str) -> BeautifulSoup:
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(text_page, 'html.parser')
     return soup
 
-def last_page(text_page: str) -> int:
-
-    soup = souping(text_page)
-
-    # Try to find the last page number from the pagination elements
-    try:            
-        last_pages = soup.find_all('span', class_='c-navigationPagination_itemButtonContent')
-        last_page_number = last_pages[-2].text.strip()
-    except: 
-        last_page_number = 1
-
-    return int(last_page_number)
-
+# Function to extract game's name and links from the HTML content
 def get_games(text_page: str) -> list:
     soup = souping(text_page)
 
@@ -36,3 +24,64 @@ def get_games(text_page: str) -> list:
         link_games.append(i.get('href'))
 
     return games, link_games
+
+def get_game_info(text_page: str) -> list:
+    soup = souping(text_page)
+
+    game_name = soup.find('h1').text    
+    platforms = soup.find_all('li', class_='c-gameDetails_listItem g-color-gray70 u-inline-block')
+    platforms = [games.text.strip() for games in platforms]
+    genres = soup.find_all('span', class_='c-globalButton_label')[-2].text.strip()
+
+    try:
+        released_date = soup.find_all('div', class_='g-text-xsmall')[1].find_all('span')[-1].text
+    except:
+        released_date = None
+
+    try:
+        developer = soup.find('li', class_='c-gameDetails_listItem u-inline-block g-color-gray70').text.strip()
+    except:
+        developer = soup.find_all('a', class_='u-text-underline')[-2].text.strip()
+    try:
+       publisher = soup.find('span', class_='g-outer-spacing-left-medium-fluid u-block g-color-gray70').text.strip()
+    except:
+        publisher = soup.find_all('a', class_='u-text-underline')[-1].text.strip()
+    
+    try:
+        metascore_review = soup.find('div', class_='c-siteReviewScore_medium').text
+        positive_critic = soup.find('div', class_='c-reviewsStats_positiveStats').find_all('span')[-1].text.split(' ')[0]
+        mixed_critic = soup.find('div', class_='c-reviewsStats_neutralStats').find_all('span')[-1].text.split(' ')[0]
+        negative_critic = soup.find('div', class_='c-reviewsStats_negativeStats').find_all('span')[-1].text.split(' ')[0]
+    except:
+        positive_critic = None
+        mixed_critic = None
+        negative_critic = None
+        metascore_review = None
+    try:
+        user_review = soup.find_all('div', class_='c-siteReviewScore_medium')[1].find('span').text
+        positive_user = soup.find_all('div', class_='c-reviewsStats_positiveStats')[2].find_all('span')[-1].text.split(' ')[0]
+        mixed_user = soup.find_all('div', class_='c-reviewsStats_neutralStats')[2].find_all('span')[-1].text.split(' ')[0]
+        negative_user = soup.find_all('div', class_='c-reviewsStats_negativeStats')[2].find_all('span')[-1].text.split(' ')[0]
+    except:
+        positive_user = None
+        mixed_user = None
+        negative_user = None
+        user_review = None
+
+    data = {'game_name': game_name,
+            'released_date': released_date,
+            'metascore_review': metascore_review,
+            'user_review': user_review,
+            'platforms': platforms,
+            'developer': developer,
+            'publisher': publisher,
+            'genres': genres,
+            'positive_critic': positive_critic,
+            'mixed_critic': mixed_critic,
+            'negative_critic': negative_critic,
+            'positive_user': positive_user,
+            'mixed_user': mixed_user,
+            'negative_user': negative_user
+            }
+    
+    return data
