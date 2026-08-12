b61c45c002b827d21f5f1d04eed84a85bf209836
Alan Mota <alanmotagomes@gmail.com>
2026-08-11 23:09:42 -0300

    <feat>(main): loop for colect data from many years

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from core.extract import fetch_page, last_page
+from tqdm import tqdm
+import os
+
 def main():
-    print("Hello from games-data!")
+
+    for year in range(2025,2026):
+        year_url = f'https://www.metacritic.com/browse/game/all/all/{year}/metascore/?page='
+        last_page_number = last_page(year_url)
+
+        for page in tqdm(range(1, last_page_number + 1), desc="Fetching pages"):
+            page_url = f'{year_url}{page}'
+            page_content = fetch_page(page_url)
+
+            if os.path.exists(f'data/{year}/game_list'):
+                with open(f'data/{year}/game_list/page_{page}.html', 'w', encoding='utf-8') as file:
+                    file.write(page_content)
+            else:
+                os.makedirs(f'data/{year}/game_list', exist_ok=True)
+                with open(f'data/{year}/game_list/page_{page}.html', 'w', encoding='utf-8') as file:
+                    file.write(page_content)
 
 
 if __name__ == "__main__":
