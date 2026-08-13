9c5c1d74659fd3213f925e4322d94de0a4d48bf7
Alan Mota <alanmotagomes@gmail.com>
2026-08-13 01:00:01 -0300

    <feat>(main.py): create function to collect the pages of some year

--- main.py
+++ main.py
@@ -2,24 +2,30 @@ from core.extract import fetch_page, last_page
 from tqdm import tqdm
 import os
 
+def collect_game_list(years: list, dir_path: str = 'data/game_list') -> str:
+    for y in years:
+            year_url = f'https://www.metacritic.com/browse/game/all/all/{y}/metascore/?page='
+            last_page_number = last_page(year_url) + 1
+    
+            for page in tqdm(range(1, last_page_number), desc="Fetching pages"):
+                page_url = f'{year_url}{page}'
+                page_content = fetch_page(page_url)
+    
+                if os.path.exists(dir_path):
+                    with open(f'{dir_path}/y_{y}_pg_{page}.html', 'w', encoding='utf-8') as file:
+                        file.write(page_content)
+                else:
+                    os.makedirs(dir_path, exist_ok=True)
+                    with open(f'{dir_path}/y_{y}_pg_{page}.html', 'w', encoding='utf-8') as file:
+                        file.write(page_content)
+
+def collect_game_page(links: list, dir_path: str = 'data/game_pages') -> str:
+     pass
+
+
+
 def main():
-
-    for year in range(2025,2026):
-        year_url = f'https://www.metacritic.com/browse/game/all/all/{year}/metascore/?page='
-        last_page_number = last_page(year_url)
-
-        for page in tqdm(range(1, last_page_number + 1), desc="Fetching pages"):
-            page_url = f'{year_url}{page}'
-            page_content = fetch_page(page_url)
-
-            if os.path.exists(f'data/{year}/game_list'):
-                with open(f'data/{year}/game_list/page_{page}.html', 'w', encoding='utf-8') as file:
-                    file.write(page_content)
-            else:
-                os.makedirs(f'data/{year}/game_list', exist_ok=True)
-                with open(f'data/{year}/game_list/page_{page}.html', 'w', encoding='utf-8') as file:
-                    file.write(page_content)
-
+     pass
 
 if __name__ == "__main__":
     main()
